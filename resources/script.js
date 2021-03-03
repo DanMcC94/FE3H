6ab635e9f848ccb9b111f8ca2ee74242115497e9
DM94 <danmccartney94@gmail.com>
2021-03-03 15:23:00 -0600

    Added to javascript and html

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,23 @@
-function myFunction() {
-  var x = document.getElementById("selector");
+function nav() {
+  var x = document.getElementById("navigator");
   if (x.style.display === "none") {
     x.style.display = "block";
+    document.getElementById("BEnav").innerHTML = "Hide Nav";
   } else {
     x.style.display = "none";
+    document.getElementById("BEnav").innerHTML = "Show Nav";
   }
 }
+
+function cmbtArts() {
+  var cmbtArts = document.getElementById("BEcmbtArts");
+  if (cmbtArts.style.display === "none") {
+    cmbtArts.style.display = "";
+    document.getElementById("BEcmbtButton").innerHTML = "Hide";
+  } else {
+    cmbtArts.style.display = "none";
+    document.getElementById("BEcmbtButton").innerHTML = "Show";
+  }
+}
+
+
